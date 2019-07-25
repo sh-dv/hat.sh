94d8158e022f86e00eef09760f43b9be4a77b6f6
shdv <ishdv@pm.me>
2019-07-25 18:23:46 +0300

    Update main.js

--- desktop-app-electronjs/main.js
+++ desktop-app-electronjs/main.js
@@ -1,10 +1,22 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path')
+const contextMenu = require('electron-context-menu');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+contextMenu({
+	prepend: (defaultActions, params, BrowserWindow) => [
+		{
+			label: 'Hat.sh :)',
+			// Only show it when right-clicking images
+			visible: params.mediaType === 'image'
+		}
+	]
+});
+
+let mainWindow;
+
 
 function createWindow () {
   // Create the browser window.
@@ -14,8 +26,6 @@ function createWindow () {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
-
-
   })
 
   // and load the index.html of the app.
@@ -30,9 +40,6 @@ function createWindow () {
     app.quit();
   })
 }
-
-
-
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -55,27 +62,12 @@ app.on('activate', function () {
 
 const template = [
   // { role: 'appMenu' }
-  ...(process.platform === 'darwin'
-    ? [
-        {
-          label: app.getName(),
-          submenu: [{ role: 'quit' }]
-        },
-        {
-          label: 'Edit',
-          submenu: [
-            { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
-            { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-            { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
-            {
-              label: 'Select All',
-              accelerator: 'CmdOrCtrl+A',
-              selector: 'selectAll:'
-            }
-          ]
-        }
-      ]
-    : [])
+  ...(process.platform === 'darwin' ? [{
+    label: app.getName(),
+    submenu: [
+      { role: 'quit' }
+    ]
+  }] : [])
 ];
 
 const menu = Menu.buildFromTemplate(template)

4528dddd0ef27fc899e34a94a8b0fae590c47c73
sh-dv <44544201+sh-dv@users.noreply.github.com>
2021-07-02 14:54:39 +0000

    Delete electron directory

--- electron/renderer.js
+++
@@ -1,8 +0,0 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-// Open all links in external browser
-mainWindow.webContents.on('new-window', function(e, url) {
-    e.preventDefault();
-    require('electron').shell.openExternal(url);
-  });

f0b29b3eefaca8725abeb4802934b5c9e1414502
sh-dv <44544201+sh-dv@users.noreply.github.com>
2021-10-03 16:03:36 +0300

    format code

--- src/utils/generatePassword.js
+++ src/utils/generatePassword.js
@@ -1,8 +1,11 @@
 const _sodium = require("libsodium-wrappers");
 
 export const generatePassword = async () => {
-    await _sodium.ready;
-    const sodium = _sodium;
-    let password = sodium.to_base64(sodium.randombytes_buf(16), sodium.base64_variants.URLSAFE_NO_PADDING);
-    return password;
-  }
+  await _sodium.ready;
+  const sodium = _sodium;
+  let password = sodium.to_base64(
+    sodium.randombytes_buf(16),
+    sodium.base64_variants.URLSAFE_NO_PADDING
+  );
+  return password;
+};

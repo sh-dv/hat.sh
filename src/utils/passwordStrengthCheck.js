f8b1f30b6fa2018b0467d1c29cca5efb9ed28d34
sh-dv <44544201+sh-dv@users.noreply.github.com>
2021-11-12 20:32:31 +0200

    Localization and translation

--- src/utils/passwordStrengthCheck.js
+++ src/utils/passwordStrengthCheck.js
@@ -1,12 +1,13 @@
 import zxcvbn from "zxcvbn";
+import { getTranslations as t} from "../../locales";
 
 const passwordStrengthCheck = (password) => {
   let strength = {
-    0: "Very Weak (guessable)",
-    1: "Weak (guessable)",
-    2: "moderate",
-    3: "Good",
-    4: "Strong",
+    0: t('very_weak'),
+    1: t('weak'),
+    2: t('moderate'),
+    3: t('good'),
+    4: t('strong'),
   };
 
   let strengthResult = zxcvbn(password);

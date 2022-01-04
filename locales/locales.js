432731ef98e21e9fff7ca78322b22ef5691977b8
sh-dv <44544201+sh-dv@users.noreply.github.com>
2022-01-05 01:20:21 +0200

    Update locale imports

--- locales/locales.js
+++ locales/locales.js
@@ -1,11 +1,11 @@
-import en from "./en";
-import fr from "./fr";
+import en_US from "./en_US";
+import fr_FR from "./fr_FR";
 import zh_CN from "./zh_CN";
 
 const locales = {
-  en,
-  fr,
-  zh_CN,
+  en_US,
+  fr_FR,
+  zh_CN
 };
 
 export default locales;

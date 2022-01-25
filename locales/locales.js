ca1e4a82113c6eac636e8103573534f2c77f834c
kinchiller <kingchiller@gmail.com>
2022-01-25 22:33:21 +0100

    Add German translation

--- locales/locales.js
+++ locales/locales.js
@@ -1,11 +1,13 @@
 import en_US from "./en_US";
 import fr_FR from "./fr_FR";
+import de_DE from "./de_DE";
 import zh_CN from "./zh_CN";
 
 const locales = {
   en_US,
   fr_FR,
-  zh_CN
+  de_DE,
+  zh_CN,
 };
 
 export default locales;

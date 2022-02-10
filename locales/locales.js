8e93babb15a60557eb514e18e4090795af1dd1d7
matteotardito <43547784+matteotardito@users.noreply.github.com>
2022-02-10 22:09:05 +0100

    Added italian translation

--- locales/locales.js
+++ locales/locales.js
@@ -2,12 +2,14 @@ import en_US from "./en_US";
 import fr_FR from "./fr_FR";
 import de_DE from "./de_DE";
 import zh_CN from "./zh_CN";
+import it_IT from "./it_IT";
 
 const locales = {
   en_US,
   fr_FR,
   de_DE,
   zh_CN,
+  it_IT,
 };
 
 export default locales;

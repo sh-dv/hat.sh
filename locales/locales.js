cb02473b0faa2d703e84152f61b349ece314d8bc
Ser-Bul <30335009+Ser-Bul@users.noreply.github.com>
2022-02-26 22:18:00 +0100

    Translation into Russian. (#57)
    * Translation into Russian.
    Besides, I see design issue: the page About should be added to the scope of translation too. Currenty when the user switch the language, the tree on the left on page About gets translated into that language, but not the page content itself.
    
    * Update for the translation into Russian:
    - updated index.js (minor corrections)
    - added docs.md
    
    Co-authored-by: Ser-Bul <agladilov@rocketsoftware.com>

--- locales/locales.js
+++ locales/locales.js
@@ -2,12 +2,14 @@ import en_US from "./en_US";
 import fr_FR from "./fr_FR";
 import de_DE from "./de_DE";
 import zh_CN from "./zh_CN";
+import ru_RU from "./ru_RU";
 
 const locales = {
   en_US,
   fr_FR,
   de_DE,
   zh_CN,
+  ru_RU,
 };
 
 export default locales;

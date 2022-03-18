97f2d2d9523bf248d19bdedc74457e12184380c2
sh-dv <44544201+sh-dv@users.noreply.github.com>
2022-03-18 10:19:52 +0000

    Update locales.js

--- locales/locales.js
+++ locales/locales.js
@@ -4,6 +4,7 @@ import de_DE from "./de_DE";
 import zh_CN from "./zh_CN";
 import ru_RU from "./ru_RU";
 import it_IT from "./it_IT";
+import sk_SK from "./sk_SK";
 import es_ES from "./es_ES";
 
 const locales = {
@@ -13,6 +14,7 @@ const locales = {
   zh_CN,
   ru_RU,
   it_IT,
+  sk_SK,
   es_ES
 };
 

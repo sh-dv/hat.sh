f7515679f76772706f33a4aede5b4d047b0140ee
xbdm <xbdm@xbdm.fun>
2023-07-20 14:25:53 -0400

    Update Language.js

--- src/config/Language.js
+++ src/config/Language.js
@@ -53,7 +53,7 @@ const Language = () => {
           severity="info"
           action={
             <Button
-              href="https://github.com/WhateverItWorks/my-hat.sh-docker-compose/blob/master/TRANSLATION.md"
+              href="https://github.com/WhateverItWorks/hat.sh-reworked/blob/master/TRANSLATION.md"
               target="_blank"
             >
               {t("guide")}
